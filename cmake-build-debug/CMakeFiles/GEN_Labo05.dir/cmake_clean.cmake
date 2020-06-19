7cfcfce4b6af43ac5ab823105bfb4393808b599f
Chiffelle <janischiffelle@hotmail.ch>
2020-06-19 21:49:15 +0200

    init projet

---
+++ cmake-build-debug/CMakeFiles/GEN_Labo05.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GEN_Labo05.dir/Customer.cpp.obj"
+  "CMakeFiles/GEN_Labo05.dir/Movie.cpp.obj"
+  "CMakeFiles/GEN_Labo05.dir/main.cpp.obj"
+  "GEN_Labo05.exe"
+  "GEN_Labo05.exe.manifest"
+  "GEN_Labo05.pdb"
+  "libGEN_Labo05.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GEN_Labo05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
